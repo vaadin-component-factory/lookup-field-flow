92c2b9a11239ebc5b71cdd4cde2049e33c96933e
Paola De Bartolo <paola.debartolo@gmail.com>
2024-11-06 16:30:40 -0300

    build: set web-component version to 4.0.0
    This version is compatible with Vaadin 24.

--- lookup-field-flow/src/main/java/com/vaadin/componentfactory/lookupfield/AbstractLookupField.java
+++ lookup-field-flow/src/main/java/com/vaadin/componentfactory/lookupfield/AbstractLookupField.java
@@ -37,7 +37,7 @@ import java.util.stream.Stream;
 @Uses(value = Button.class)
 @Tag("vcf-lookup-field")
 @JsModule("@vaadin-component-factory/vcf-lookup-field")
-@NpmPackage(value = "@vaadin-component-factory/vcf-lookup-field", version = "24.0.0")
+@NpmPackage(value = "@vaadin-component-factory/vcf-lookup-field", version = "4.0.0")
 @CssImport(value = "./lookup-dialog-themes.css")
 public abstract class AbstractLookupField<T, SelectT, ComboboxT extends HasEnabled & HasValidation & HasSize & HasValue<?, SelectT>,
         ComponentT extends AbstractLookupField<T, SelectT, ComboboxT, ComponentT, FilterType>, FilterType> extends Div
